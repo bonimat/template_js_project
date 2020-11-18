191058dba31662ab26549c69e4e84a71b5849aac
Matteo Boni <bonimat@hotmail.com>
2020-11-18 06:59:21 +0100

    Add first test

---
+++ test/somefile.test.js
@@ -0,0 +1,7 @@
+var assert = require('assert');
+describe('Test Application', function () {
+    it('should return -1 when the value is not present', function () {
+        const valueToCheck = true;
+        assert.strictEqual(valueToCheck, true, 'Good! It works!');
+    });
+});
